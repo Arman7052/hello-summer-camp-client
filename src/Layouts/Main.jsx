8e47379bf84d343ebc2b0d58d9ce7a2951540082
Arman7052 <99arman7052@gmail.com>
2023-06-08 21:16:40 +0600

    Footer Done

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Shared/Footer";
 
 
 const Main = () => {
     return (
         <div className=' max-w-7xl mx-auto'>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
